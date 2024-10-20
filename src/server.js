3f09bfcce9e618ba73daa2cd96b7800fec4d3c1b
HelenYazh <olenka.yazh05@gmail.com>
2024-10-20 23:51:58 +0300

    add get contact

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 dotenv.config();
 
@@ -19,6 +20,34 @@ export const setupServer = () => {
             },
         }),
     );
+
+    app.get("/contacts", async (req, res) => {
+        const contacts = await getAllContacts();
+
+        res.status(200).json({
+            message: "Successfully found contacts!",
+            data: contacts,
+        });
+    });
+
+    app.get('/contacts/:contactId', async (req, res, next) => {
+        const { contactId } = req.params;
+
+        const contact = await getContactById(contactId);
+
+        if (!contact) {
+            res.status(404).json({
+                message: 'Contact not found'
+            });
+            return;
+        }
+
+
+        res.status(200).json({
+            message: `Successfully found contact with id ${contactId}!`,
+            data: contact,
+        });
+    });
 
     app.use("*", (req, res, next) => {
         res.status(404).json({
