73529c9ecb72b7abcd9895d5071ab975b5a1b24b
HelenYazh <olenka.yazh05@gmail.com>
2024-10-21 11:43:51 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -25,6 +25,7 @@ export const setupServer = () => {
         const contacts = await getAllContacts();
 
         res.status(200).json({
+            status: 200,
             message: "Successfully found contacts!",
             data: contacts,
         });
@@ -44,6 +45,7 @@ export const setupServer = () => {
 
 
         res.status(200).json({
+            status: 200,
             message: `Successfully found contact with id ${contactId}!`,
             data: contact,
         });
