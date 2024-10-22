74da5c26a3aeb6088adf0cfd48b8669a0991404e
HelenYazh <olenka.yazh05@gmail.com>
2024-10-22 15:16:55 +0300

    add patch method

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
-import { createContact, deleteContact, getAllContacts, getContactById } from "../services/contacts.js";
+import { createContact, deleteContact, getAllContacts, getContactById, updateContact } from "../services/contacts.js";
 
 export const getContactsController = async (req, res) => {
     const contacts = await getAllContacts();
@@ -36,6 +36,24 @@ export const createContactController = async (req, res, next) => {
         status: 201,
         message: "Successfully created a contact!",
         data: contact,
+    });
+};
+
+
+export const patchContactController = async (req, res, next) => {
+    const { contactId } = req.params;
+
+    const result = await updateContact(contactId, req.body);
+
+    if (!result) {
+        next(createHttpError(404, "Contact not found"));
+        return;
+    }
+
+    res.json({
+        status: 200,
+        message: "Successfully patched a contact!",
+        data: result.contact,
     });
 };
 
