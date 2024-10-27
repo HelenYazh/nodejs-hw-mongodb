e1542c53c5fe82b0d6f788ff63b17dc8b32a66e8
HelenYazh <olenka.yazh05@gmail.com>
2024-10-27 23:57:28 +0200

    pagination

---
+++ src/utils/calculatePaginationData.js
@@ -0,0 +1,15 @@
+export const calculatePaginationData = (count, page, perPage) => {
+    const totalPage = Math.ceil(count / perPage);
+    const hasNextPage = Boolean(totalPage - page);
+    const hasPreviousPage = page !== 1;
+
+    return {
+        page,
+        perPage,
+        totalItems: count,
+        totalPage,
+        hasNextPage,
+        hasPreviousPage,
+    };
+};
+
