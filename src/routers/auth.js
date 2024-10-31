5000c62fc8b42c1ed4a507426afe1478c9aa7e2d
HelenYazh <olenka.yazh05@gmail.com>
2024-10-31 13:52:07 +0200

    add refresh

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUserController, logoutUserController, registerUserController } from "../controllers/auth.js";
+import { loginUserController, logoutUserController, refreshUserSessionController, registerUserController } from "../controllers/auth.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { loginUserSchema, registerUserSchema } from "../validation/auth.js";
@@ -10,5 +10,6 @@ const router = Router();
 router.post("/register", validateBody(registerUserSchema), ctrlWrapper(registerUserController));
 router.post("/login", validateBody(loginUserSchema), ctrlWrapper(loginUserController));
 router.post("/logout", ctrlWrapper(logoutUserController));
+router.post("/refresh", ctrlWrapper(refreshUserSessionController));
 
 export default router;
